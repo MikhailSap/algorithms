f3e754f86c42b65fe0b226a4a9fc50e4f3c1e256
Mikhail <vandaniel@mail.ru>
2020-01-26 22:14:17 +0300

    hashmap

---
+++ lesson-8/src/main/java/lesson8/Main.java
@@ -0,0 +1,28 @@
+package lesson8;
+
+import lesson8.map.MyHashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+
+        System.out.println(map);
+        System.out.println(map.size());
+
+        System.out.println(map.remove("two"));
+
+        System.out.println(map);
+        System.out.println(map.size());
+
+        for (int i = 0; i < 1000; i++) {
+            map.put(String.valueOf(i), i);
+        }
+
+        System.out.println(map.size());
+        System.out.println(map);
+    }
+}
